69b8d20587b1bca0f727ad59516b661adad2a90a
khalidwalidanwar <86128823+khalidwalidanwar@users.noreply.github.com>
2024-12-29 20:34:44 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -17,7 +17,7 @@ overlay.onclick = ()=>{
     popup.style.scale = 0;
     overlay.style.scale = 0;
 }
-var inputFile = document.querySelector("#upload");
+var inputFile = document.querySelector("#fileInput");
 var filename = document.querySelector(".filename");
 document.querySelector(".upload").onclick = ()=>{
     inputFile.click();
@@ -34,12 +34,39 @@ inputFile.onchange=()=>{
 import {adddposts} from "./app.js";
 
 document.querySelector(".askSomeForm button.sendQ").addEventListener("click",(e)=>{
-    var newPost = document.querySelector(".askSomeForm textarea").value;
+    var newPost = document.querySelector(".askSomeForm textarea");
     var showUser = document.querySelector(".askSomeForm input[type='checkbox']").checked;
     var newCategory = document.querySelector(".askSomeForm select").value;
-    var newImage = document.querySelector(".askSomeForm input[type='file']").value;
-    if(newPost && newCategory){
-        adddposts(newPost,showUser,newCategory,newImage);
+    var fileInput = document.querySelector(".askSomeForm input[type='file']");
+    var newImage = '';
+    if(newPost.value && newCategory){
+    const cloudName = "dfochp65f"; // Replace with your Cloudinary cloud name
+    const uploadPreset = "my-upload-preset"; // Replace with your upload preset (configured in Cloudinary)
+        if (fileInput.files.length > 0) {
+            const file = fileInput.files[0];
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("upload_preset", uploadPreset);
+            // Use Cloudinary API to upload the image
+            newPost.parentElement.classList.add("unload");
+            fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
+            method: "POST",
+            body: formData,
+            })
+            .then(response => response.json())
+            .then(data => {
+                newImage = data.secure_url; // URL of the uploaded image
+                adddposts(newPost,showUser,newCategory,newImage);
+            // console.log("File uploaded successfully: ", imageUrl);
+            // Display the uploaded image
+            // imageDisplay.src = imageUrl;
+            })
+            .catch(error => {
+            console.error("Error uploading image: ", error);
+            });
+        }else{
+            newImage = '';
+        }
     }else{
         document.querySelector(".askSomeForm p.error").innerHTML = "من فضلك اختر المادة و اكتب سؤالك";
     }
